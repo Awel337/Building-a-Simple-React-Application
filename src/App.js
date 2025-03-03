7abf826f8b974b2f886116c76762ed90c4ca69a9
Awel337 <seya4654@aol.com>
2025-03-03 13:28:00 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-function Header() {
-  return (<h1>Simple React Application</h1>);
-}
-
-function Content(props) {
-  return (<p style={{ color: props.color }}>{props.text}</p>);
-}
-
-function Footer() {
-  return (<h1>Created by Me, of course.</h1>);
-}
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
